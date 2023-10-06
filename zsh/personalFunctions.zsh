b4b5e2b37e768140ae44b4b0c87195dfd350984f
bGZo <im@bgzo.cc>
2023-10-06 13:31:22 +0800

    feat: union the history from ubuntu -> wsl2 -> arch.
    the details is following:
        1. update the dependency
        2. remove useless function
        3. fix the submodules not tracked
        4. todo: want to auto backup dotfiles with `auto.sh`

--- zsh/personalFunctions.zsh
+++ zsh/personalFunctions.zsh
@@ -3,24 +3,62 @@
 #+-------+
 #|General|
 #+-------+
-
-function error_exit {
+# NOTE: `function` & `()` is same thing.
+error_exit() {
     # 1>&2 redirect stderr to stdout, see via
     # https://stackoverflow.com/questions/818255/in-the-shell-what-does-21-mean#answer-16283739
     echo "$1" 1>&2
     exit
 }
 
-#+---------+
-#|Inpendent|
-#+---------+
+#+-----------------------+
+#|fix: History Commandline|
+#+-----------------------+
+fixNotReadingHistory(){
+    # via:      https://shapeshed.com/zsh-corrupt-history-file
+    # license:  http://unlicense.org
+    mv ~/.zsh_history ~/.zsh_history_bad
+    strings -eS ~/.zsh_history_bad > ~/.zsh_history
+    fc -R ~/.zsh_history
+    rm ~/.zsh_history_bad
+}
 
-openProxy(){
-    host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+#+---------------------------------+
+#|feat: switch on GNOME Environment|
+#+---------------------------------+
+swithThemeOnGnome() {
+    out=$(gsettings get org.gnome.desktop.interface gtk-theme) || error_exit "Not Found Theme"
+    if [ $out = \'WhiteSur-dark-solid\' ]; then 
+        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light-solid"
+    elif [ $out = \'WhiteSur-light-solid\' ]; then
+        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark-solid"
+    fi
+}
+
+#+-----------+
+#|feat: Proxy|
+#+-----------+
+proxyOnTerminal(){
+    local host_ip="$1"
+    if [ -z "$host_ip" ]; then
+        echo "Usage: proxyOnTerminal <host_ip>"
+        return 1
+    fi
+
+    # FIXME: does shell have callback function to callback output to docker?
     export all_proxy="http://$host_ip:7890"
     export http_proxy="http://$host_ip:7890"
     export https_proxy="http://$host_ip:7890"
-    
+    echo "Proxy is ON."
+}
+
+proxyOnDocker(){
+    local host_ip="$1"
+    if [ -z "$host_ip" ]; then
+        echo "Usage: proxyOnDocker <host_ip>"
+        return 1
+    fi
+
     echo "{
 \"proxies\":
 {
@@ -33,64 +71,64 @@ openProxy(){
    }
 }
 }" > $HOME/.docker/config.json
-
-
-    echo "HTTP_PROXY=http://$host_ip:7890" > $HOME/ttrss/.env
-    cat /mnt/c/Users/15517/bin/lan_ip >> $HOME/ttrss/.env
-    echo "HTTP Proxy on"
-    out-ip-bin
+    # FIXME: depand out-ip-bin
+    # echo "all_proxy=http://$host_ip:7890" > $HOME/ttrss/.env
+    # echo "http_proxy=http://$host_ip:7890" >> $HOME/ttrss/.env
+    # echo "https_proxy=http://$host_ip:7890" >> $HOME/ttrss/.env
+    # cat /mnt/c/Users/15517/bin/lan_ip >> $HOME/ttrss/.env
 }
 
 closeProxy() {
-    unset ALL_PROXY
-    echo "HTTP Proxy off"
+    unset all_proxy
+    unset http_proxy
+    unset https_proxy
+
+    echo "Proxy is OFF."
 }
 
-fixNotReadingHistory(){
-    # Idea from https://shapeshed.com/zsh-corrupt-history-file
-    # License - http://unlicense.org
-    mv ~/.zsh_history ~/.zsh_history_bad
-    strings -eS ~/.zsh_history_bad > ~/.zsh_history
-    fc -R ~/.zsh_history
-    rm ~/.zsh_history_bad
-}
+openProxy(){
+    # Judge this machine is WSL/WSL2, to use different proxy;
+    if [[ $(uname -r) == *Microsoft* || $(uname -r) == *WSL* ]]; then
+        echo "This is a WSL system."
+        host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+    else
+        echo "This is not a WSL system. Great )"
+        host_ip="127.0.0.1"
+    fi
 
-gitCommit(){
-    git add .
-    git status
-    echo "[bGZoCg] Input something:"
-    read git_tmp_ver # ah,zsh do not support readline(arrow key, -e in bash)
-    git commit -m $git_tmp_ver
+    proxyOnTerminal $host_ip
 }
 
 openClash() {
-    clashDir=(where clash) || error_exit "Cannot find path of clash, set it please."
-    .$clashDir/clash -d .
+    proxyDir="$HOME/workspace/proxy"
+    # NOTE!: not proxyDir="~/workspace/proxy"
+    assertClash=(where clash) || error_exit "Cannot find path of clash, set it please."
+
+    clash -d $proxyDir
 }
 
-openXAMPP(){
-    sudo ./opt/lampp/manager-linux-x64.run || error_exit "Not Found Software"
-}
 
-openCAJVIEWER(){
-    .~/opt/cajviewer.AppImage || error_exit "Not Found Software"
-} 
+# gitCommit(){
+#     git add .
+#     git status
+#     echo "[bGZoCg] Input something:"
+#     read git_tmp_ver # ah,zsh do not support readline(arrow key, -e in bash)
+#     git commit -m $git_tmp_ver
+# }
 
-swithTheme() {
-    out=$(gsettings get org.gnome.desktop.interface gtk-theme) || error_exit "Not Found Theme"
-    if [ $out = \'WhiteSur-dark-solid\' ]; then 
-        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light-solid"
-    elif [ $out = \'WhiteSur-light-solid\' ]; then
-        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark-solid"
-    fi
-}
 
-out-ip-bin() {
-     ip addr show eth0 | grep 'inet ' | cut -f 6 -d ' ' | cut -f 1 -d '/' > /mnt/c/Users/15517/bin/wsl_ip
-}
-
-open-gui-server(){
-    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-    export LIBGL_ALWAYS_INDIRECT=1
-    sudo /etc/init.d/dbus start &> /dev/null
-}
+# openXAMPP(){
+#     sudo ./opt/lampp/manager-linux-x64.run || error_exit "Not Found Software"
+# }
+# openCAJVIEWER(){
+#     .~/opt/cajviewer.AppImage || error_exit "Not Found Software"
+# } 
+#
+# out-ip-bin() {
+#     ip addr show eth0 | grep 'inet ' | cut -f 6 -d ' ' | cut -f 1 -d '/' > /mnt/c/Users/15517/bin/wsl_ip
+# }
+# open-gui-server(){
+#     export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+#     export LIBGL_ALWAYS_INDIRECT=1
+#     sudo /etc/init.d/dbus start &> /dev/null
+# }
